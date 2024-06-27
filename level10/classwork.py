52e4cfeefcb1cc4a193a98f24466757d71eff050
just0mel <just.bud.k21@gmail.com>
2024-06-27 23:47:01 +0400

    second commit

---
+++ level10/classwork.py
@@ -0,0 +1,9 @@
+for number in range(50,100):
+     print(number)
+
+for number1 in range(10,0,-1):
+     print(number1)
+
+
+for number2 in range(100):
+     print("hello world")
---
+++ level6/homework.py
@@ -0,0 +1,9 @@
+num1 = 100
+num2 = 5
+num3 = 2
+
+print(num1 + num2)
+print(num1 - num3)
+print(num1 // num2)
+print(num1 * num3)
+print(num2 ** num3)
