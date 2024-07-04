d22e22109a4b2918594777e34c8d758d285f90f1
just0mel <just.bud.k21@gmail.com>
2024-07-04 23:05:15 +0400

    second commit

---
+++ level10/homework.py
@@ -0,0 +1,4 @@
+a= 1
+while a < 1001:
+    print(a)
+    a += 1
---
+++ level8/homework.py
@@ -0,0 +1,44 @@
+x=7
+y=9
+z=2
+w=7
+n=5
+m=2
+v=13
+k=18
+
+# ==
+print(m==x)#false
+print(z==k)#false
+print(x==w)#true
+print(y==n)#false
+
+# !=
+print(y!=n)#true
+print(k!=x)#true
+print(w!=x)#false
+print(z!=n)#true
+
+# >=
+print(y>=x)#true
+print(k>=v)#true
+print(z>=v)#false
+print(x>=w)#false
+
+# <=
+print(z<=k)#true
+print(w<=x)#true
+print(y<=m)#false
+print(v<=k)#true
+
+# >
+print(x>z)#true
+print(m>k)#false
+print(n>w)#false
+print(v>y)#true
+
+# <
+print(k<y)#false
+print(x<w)#false
+print(z<n)#true
+print(v<n)#false
